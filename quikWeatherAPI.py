4a1b54c0435df55f981dfd1f262de3ec2127cdf2
trainboy2019 <ikerolader@gmail.com>
2023-04-18 15:19:18 -0400

    census api test

--- quikWeatherAPI.py
+++ quikWeatherAPI.py
@@ -13,3 +13,14 @@ weatherStr2=weatherBytes2.decode('utf8')
 weather2.close()
 weatherJson2=json.loads(weatherStr2)
 print(weatherJson2["properties"]["periods"][0]["shortForecast"])
+
+with open('censusKey.txt') as f:
+    censusKey=f.read()
+state=6
+#censusStateTable=urllib.request.urlopen(urllib.request.Request(f'https://api.census.gov/data/2021/acs/acs1?get=NAME&for=state&key={censusKey}'))
+census=urllib.request.urlopen(urllib.request.Request(f'https://api.census.gov/data/2021/acs/acs1?get=NAME,B01001_001E&for=place&in=state:{str(state).zfill(2)}&key={censusKey}'))
+censusBytes=census.read()
+censusStr=censusBytes.decode('utf8')
+census.close()
+censusJson=json.loads(censusStr)
+print(censusJson)
