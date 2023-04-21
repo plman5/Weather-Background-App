0136e0d436ba1c9374da89cf6e62e4b4a6430113
CJ Boni <56044490+Halfbrit@users.noreply.github.com>
2023-04-21 17:07:42 -0400

    add census call functionality for city population

--- apiCalls.py
+++ apiCalls.py
@@ -1,6 +1,7 @@
 import http.client, urllib.parse
 import json
 import requests
+import urllib.request
 
 def getCityCoords(city: str, state: str) -> list:
     conn = http.client.HTTPConnection('api.positionstack.com')
@@ -16,7 +17,6 @@ def getCityCoords(city: str, state: str) -> list:
 
     res = conn.getresponse()
     data = json.load(res)
-    print(data)
     return [data['data'][0]['latitude'], data['data'][0]['longitude']]
 
 def getWeatherInfo(latitude: float, longitude: float) -> str:
@@ -29,3 +29,16 @@ def getWeatherInfo(latitude: float, longitude: float) -> str:
     response = requests.get(forecastURL)
     res = response.json()
     return res['properties']['periods'][0]['shortForecast']
+
+def getPopulationInfo(city: str, state: str)->float:
+    census=urllib.request.urlopen(urllib.request.Request(f'https://api.census.gov/data/2021/acs/acs1?get=NAME,B01001_001E&for=place&in=state:{str(state).zfill(2)}&key={censusKey}'))
+    censusBytes=census.read()
+    censusStr=censusBytes.decode('utf8')
+    census.close()
+    censusJson=json.loads(censusStr)
+
+    for cityArr in censusJson:
+        if cityArr[0] == city:
+            return cityArr[1]
+        
+    return 0
